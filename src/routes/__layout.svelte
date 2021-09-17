256868209cc70aef9d1983df2aa8163cce742ab1
tylod <tylodesu@gmail.com>
2021-09-17 19:06:41 +0300

    Added layout page

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import {onMount} from "svelte";
+
+	// Ready state controller
+	let ready = false;
+	onMount(() => ready = true);
+</script>
+
+{#if ready}
+    <slot/>
+{:else}
+    Загрузка...
+{/if}
