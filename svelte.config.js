76df288a03827f88c93d74077f880b70b78b7f4c
tylod <tylodesu@gmail.com>
2021-09-17 17:14:26 +0300

    Added SvelteKit config

---
+++ svelte.config.js
@@ -0,0 +1,12 @@
+import sveltePreprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+export default {
+    kit: {
+        adapter: adapter({}),
+        target: '#svelte',
+        ssr: false
+    },
+    preprocess: sveltePreprocess()
+};
